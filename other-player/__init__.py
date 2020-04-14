84851baef7c14aa3de4f4f7daab2575d3a8c00fa
maxh11 <max.hollands5253@gmail.com>
2020-04-14 20:55:26 +1000

    started making a greedy opponent and made data records

--- other-player/__init__.py
+++ other-player/__init__.py
@@ -5,4 +5,4 @@
 # example import below, you can define it in another file and import
 # it into this module with the name 'Player':
 
-from .player import RandomPlayer as Player
+from .player import GreedyPlayer as Player
